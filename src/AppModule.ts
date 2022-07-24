731c238bb014a514f19b058e5fd26bc8b2dd9040
Youngwoong You <asdfg13232@naver.com>
2022-07-24 18:10:57 +0900

    chore: Add validation pipe
    - validation pipe 추가

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '@/modules/users/UserModule';
 import { FormModule } from '@/modules/forms/FormModule';
 import { DatabaseConfigModule, DatabaseConfigService } from '@/config/database';
 import { TagModule } from '@/modules/tags/TagModule';
+import { ValidationPipe } from '@/common/pipes';
 
 @Module({
     imports: [
@@ -19,6 +21,11 @@ import { TagModule } from '@/modules/tags/TagModule';
         TagModule,
     ],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_PIPE,
+            useClass: ValidationPipe,
+        },
+    ],
 })
 export class AppModule {}
