7f0764fc69b58d823ff75c7b8fa9e2e910c9347c
Youngwoong You <asdfg13232@naver.com>
2022-09-19 17:38:12 +0900

    feat: Add convert video function
    - 모든 비디오 데이터를 h264 코덱으로 변환하는 부분 추가
    - 웹 환경에서 원활한 재생을 위함

--- src/common/utils/VideoConverter.ts
+++ src/common/utils/VideoConverter.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const ffmpeg = require('fluent-ffmpeg');
 
 @Injectable()
 export class VideoConverter {
-    constructor() {}
-
     getThumbnail(tmpPath: string, video: Express.Multer.File): Promise<string> {
         if (!video.mimetype.includes('video')) {
             console.log('Video file required.');
@@ -28,6 +27,31 @@ export class VideoConverter {
                     filename: name,
                     folder: './tmp',
                 });
+        });
+    }
+
+    convertToH264(tmpPath: string, video: Express.Multer.File): Promise<string> {
+        if (!video.mimetype.includes('video')) {
+            console.log('Video file required');
+            return;
+        }
+
+        const name = `tmp-convert-${+new Date()}.mp4`;
+
+        return new Promise((resolve, reject) => {
+            ffmpeg(tmpPath)
+                .on('end', () => {
+                    console.log('Convert finished');
+                    resolve(`./tmp/${name}`);
+                })
+                .on('error', (err) => {
+                    reject(err);
+                })
+                .withOutputFormat('mp4')
+                .videoCodec('libx264')
+                .outputOption(['-c:a copy', '-crf 18'])
+                .output(`./tmp/${name}`)
+                .run();
         });
     }
 }
